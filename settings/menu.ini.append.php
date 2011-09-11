9083f711e1b838432184baa9c089e3c3f9054786
Olav Frengstad <olav@fwt.no>
2011-09-11 02:56:28 +0200

    Fix missing info from ezflow/ezwebin

---
+++ settings/menu.ini.append.php
@@ -0,0 +1,11 @@
+<?php /**
+# Make it NOT print out errors becouse of missing eZ Webin / eZ Flow
+[MenuSettings]
+AlwaysAvailable=enabled
+AvailableMenuArray[]
+
+[MenuContentSettings]
+ExtraMenuSubitemsCheck=disabled
+
+*/
+?>
