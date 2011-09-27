a32cbb17d469b84d7564f64978a39cdc49600d94
Olav Frengstad <olav@fwt.no>
2011-09-27 19:50:25 +0200

    Added en_US locale

---
+++ translations/eng-US/translation.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB">
+<context>
+	<name>index.php</name>
+	<message>
+		<source>Some errors occurred, see debug for more information.</source>
+		<translation>Some errors occurred, see debug for more information.</translation>
+	</message>
+</context>
+<context>
+    <name>content/fwt/sorting</name>
+    <message>
+        <source>Sort by %type</source>
+        <translation>Sort content by %type</translation>
+    </message>
+    <message>
+        <source>Sort by %priority</source>
+        <translation>Sort content by %priority</translation>
+    </message>
+    <message>
+        <source>Order %order</source>
+        <translation>Order %order</translation>
+    </message>
+</context>
+<context>
+	<name>kernel/navigationpart</name>
+	<message>
+		<source>eZFind</source>
+		<translation>eZFind</translation>
+	</message>
+</context>
+</TS>
